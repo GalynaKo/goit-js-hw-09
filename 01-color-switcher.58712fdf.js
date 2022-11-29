d472940e1aa8674418dd7f49a26ad2aa65d21f57
GalynaKo <108280614+GalynaKo@users.noreply.github.com>
2022-11-29 18:40:01 +0000

    Deploying to gh-pages from @ GalynaKo/goit-js-hw-09@35366581242c1bb8a276b7767ae36d683d334509 🚀

--- 01-color-switcher.58712fdf.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".data-start"),t=document.querySelector(".data-stop");let l=null;e.addEventListener("click",(()=>{l=setInterval((()=>{console.log(`I love async JS!  ${Math.random()}`)}),1e3)})),t.addEventListener("click",(()=>{clearInterval(l),console.log(`Interval with id ${l} has stopped!`)}));
-//# sourceMappingURL=01-color-switcher.58712fdf.js.map
