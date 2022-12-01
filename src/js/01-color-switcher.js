ff1afa200fdae7b6dc33af5aaaf921c90288e9f1
Galyna K <alinaamodeo@gmail.com>
2022-12-01 14:09:36 +0100

    1 ok 2,3 not

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,41 @@
 //Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body>
 // на случайное значение используя инлайн стиль. При нажатии на кнопку «Stop»,
 // изменение цвета фона должно останавливаться.
-const startBtn = document.querySelector('.data-start');
-const stopBtn = document.querySelector('.data-stop');
 
-startBtn.addEventListener('click', () =>{
-  getRandomHexColor = setInterval(() => {
-console.log('startBtn');
-  },1000);
-})
-stopBtn.addEventListener("click", () => {
-  clearInterval(timerId);
-  //console.log(`Interval with id ${timerId} has stopped!`);
-});
-//Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна (disabled).
-//const handleClick = () => {
- // console.log("Button was clicked");
-//};
+ const refs = {
+  startBtn: document.querySelector('[data-start]'),
+ stopBtn: document.querySelector('[data-stop]'),
+ body: document.querySelector('body')
 
-//button.addEventListener("click", handleClick);
-//Для генерации случайного цвета используй функцию getRandomHexColor.
+ }
+ 
+ //Для генерации случайного цвета используй функцию getRandomHexColor.
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
  }
+
+//Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна (disabled).
+//
+//change its color
+let color;
+// function start
+ const startOn = () => {
+  color = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();//come arrivare al bgColor e cambiarlo
+
+  },1000)
+  refs.startBtn.setAttribute('disabled', true);
+}
+const stopOn = () => {
+  refs.startBtn.disabled = false;
+
+  clearInterval(color);
+  
+};
+
+refs.startBtn.addEventListener('click', startOn);
+refs.stopBtn.addEventListener('click', stopOn);
+
 
  
